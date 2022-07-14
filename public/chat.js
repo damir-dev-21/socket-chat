dab2fdb5947fe4feaf349141e3578c4f452d54d0
damir-dev-21 <devdamiricher@gmail.com>
2022-07-14 10:32:19 +0500

    initial

---
+++ public/chat.js
@@ -0,0 +1,39 @@
+$(function(){
+    var socket = io.connect("http://localhost:3000")
+
+    var message = $("#message");
+    var username = $("#username");
+    var send_message = $("#send_message");
+    var send_username = $("#send_username");
+    var chatroom = $("#chatroom");
+    var feedback = $("#feedback");
+
+    send_message.click(function () {
+        socket.emit('new_message',{message:message.val()})
+    })
+
+    socket.on("add_mess", data => {
+        feedback.html("");
+        message.val("");
+        chatroom.append(
+          "<div class='alert alert-" +
+            data.className +
+            "'<b>" +
+            data.username +
+            "</b>: " +
+            data.message +
+            "</div>"
+        );
+      });
+
+    socket.on('new_message',(data)=>{
+        feedback.html('')
+        message.val('')
+        chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+    })
+
+    send_username.click(()=>{
+        socket.emit('change_username',{username: username.val()})
+    })
+
+})
